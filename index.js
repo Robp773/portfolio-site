3ac4392ee89b448c66e749705dae504882f6956b
Robp773 <robpeterman573@gmail.com>
2018-08-27 13:59:27 -0400

    changed downBtn for mobile and desktop to be more responsive

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 'use strict';
-$(document).ready(function() {
-  $('.downBtn').on('click', function(event) {
-    event.preventDefault();
-    let target = event.currentTarget.hash;
-    $('html, body').animate(
-      {
-        scrollTop: $(target).offset().top
-      },
-      1000
-    );
-  });
-
-  $('.returnBtn').on('click', function(event) {
+$(document).ready(function () {
+  $('.desktopDownBtn, .downBtn, .returnBtn, #headerDownBtn').on('click', function (event) {
     event.preventDefault();
     let target = event.currentTarget.hash;
     $('html, body').animate(
